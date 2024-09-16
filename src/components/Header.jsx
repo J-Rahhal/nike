a82e728c98f7bf1f426c298d1b9a05932ba75ed5
Joanna Rahhal <joanna.h.rahhal@gmail.com>
2024-09-16 11:46:06 +0300

    Made some minor adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,16 @@ const Header = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-3 relative">
       <div className="w-full object-cover">
-        <img src="../../src/assets/header/header.jpg" alt="" />
+        <img src="../../src/assets/header/header.jpg" alt="airforce" />
       </div>
-      <div className="flex flex-col items-center justify-center gap-4 absolute text-white">
-        <p className="text-sm font-semibold md:text-xl">Nike Dunk</p>
+      <div className="flex flex-col items-center justify-center gap-4 absolute text-black lg:gap-12">
+        <p className="text-sm font-semibold md:text-xl lg:text-3xl">
+          Nike Dunk
+        </p>
         <h2 className="text-2xl font-extrabold md:text-5xl lg:text-6xl">
           STEP UP FOR YOUR CITY
         </h2>
-        <p className="text-xs w-3/4 text-center md:text-xl">
+        <p className="text-xs w-3/4 text-center md:text-xl lg:text-2xl">
           Jalen Brunson and the Nike Dunk are bringing that fire back to the
           concrete jungle
         </p>
